3c2ef29f6e495ba53ebdc379c504ae426b802653
gagandotexe-git <gagandotexe@gmail.com>
2025-10-22 06:48:39 +0000

    category

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import CategoryBar from "@/components/CategoryBar";
+import GridExample from "@/components/GridExample";
 
 export default function Home() {
   return (
     <>
+      <CategoryBar />
+      <GridExample />
     </>
   );
 }
